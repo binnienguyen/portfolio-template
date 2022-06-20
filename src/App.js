cee0828341a4b6c1d526f45717bde12265375b22
Binnie <lananh4415@gmail.com>
2022-06-20 09:23:11 +0700

    init-code

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
             cache: false,
             success: function (data) {
                 this.setState({sharedData: data});
-                document.title = `${this.state.sharedData.basic_info.name}`;
+                document.title = `${data.basic_info.name}`;
             }.bind(this),
             error: function (xhr, status, err) {
                 alert(err);
